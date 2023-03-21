a28ba9dbeebc1bc213909fa1ab9d1a9488807f70
Atlias <2283936767@qq.com>
2023-03-21 10:00:20 +0800

    用于服务器测试原始SR3对比

--- eval.py
+++ eval.py
@@ -6,7 +6,7 @@ import glob
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', type=str,
-                        default='experiments/basic_sr_ffhq_210809_142238/results')
+                        default=r'C:\Users\Atlias\Desktop\结果测试对比\ffhq_64_256\46')
     args = parser.parse_args()
     real_names = list(glob.glob('{}/*_hr.png'.format(args.path)))
     fake_names = list(glob.glob('{}/*_sr.png'.format(args.path)))
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     for rname, fname in zip(real_names, fake_names):
         idx += 1
         ridx = rname.rsplit("_hr")[0]
-        fidx = rname.rsplit("_sr")[0]
+        fidx = fname.rsplit("_sr")[0]
         assert ridx == fidx, 'Image ridx:{ridx}!=fidx:{fidx}'.format(
             ridx, fidx)
 

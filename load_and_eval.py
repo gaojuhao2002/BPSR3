4a3ab08ff3f5b476217e51e0a17b3775323b1117
Atlias <2283936767@qq.com>
2023-03-26 10:31:13 +0800

    00

--- load_and_eval.py
+++ load_and_eval.py
@@ -12,14 +12,19 @@ import logging
 import core.logger as Logger
 import core.metrics as Metrics
 import os
-
-def load_and_infer(ckpt_path,result_path,val_len,infer_step):
+def mkdirs(paths):
+    if isinstance(paths, str):
+        os.makedirs(paths, exist_ok=True)
+    else:
+        for path in paths:
+            os.makedirs(path, exist_ok=True)
+def load_and_infer(ckpt_path,val_len):
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', type=str, default='config/infer_test.json',
                         help='JSON file for configuration')
     parser.add_argument('-p', '--phase', type=str, choices=['val'], help='val(generation)', default='val')
     parser.add_argument('-gpu', '--gpu_ids', type=str, default=None)
-    parser.add_argument('-debug', '-d', action='store_true')
+    parser.add_argument('-debug', '-d', action='store_false')
     parser.add_argument('-log_infer', action='store_true')
     parser.add_argument('-enable_wandb', action='store_true')
 
@@ -29,9 +34,10 @@ def load_and_infer(ckpt_path,result_path,val_len,infer_step):
     # Convert to NoneDict, which return None for missing key.
     opt = Logger.dict_to_nonedict(opt)
     opt['path']['checkpoint']=ckpt_path#变成ckpt的路径
+    print('*-'*60,opt['path']['checkpoint'])
     opt['datasets']['val']['data_len']=val_len
-    opt['model']['beta_schedule']['n_timestep']=infer_step
-    print('-*'*20,'测试集数据长度：',opt['datasets']['val']['data_len'])
+
+    # print('-*'*20,'测试集数据长度：',opt['datasets']['val']['data_len'])
     # logging
     torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark = True
@@ -58,11 +64,12 @@ def load_and_infer(ckpt_path,result_path,val_len,infer_step):
         opt['model']['beta_schedule']['val'], schedule_phase='val')
 
     logger.info('Begin Model Inference.')
-    current_step = 0
-    current_epoch= 0
+    # current_step = opt['iter']
+    # current_epoch= opt['epoch']
     idx = 0
     avg_psnr = 0.0
     avg_ssim = 0.0
+    print('测试集数量:',len(val_loader))
     # os.makedirs(result_path, exist_ok=True)
     for _, val_data in enumerate(val_loader):
         idx += 1
@@ -71,26 +78,23 @@ def load_and_infer(ckpt_path,result_path,val_len,infer_step):
         visuals = diffusion.get_current_visuals(need_LR=False)
 
         hr_img = Metrics.tensor2img(visuals['HR'])  # uint8
-        Metrics.save_img(
-            Metrics.tensor2img(visuals['SR'][-1]), '{}/{}_{}_sr.png'.format(result_path, current_step, idx))
-        Metrics.save_img(
-            hr_img, '{}/{}_{}_hr.png'.format(result_path, current_step, idx))
 
         logger.info('Begin Model Evaluation.')
 
         eval_psnr = Metrics.calculate_psnr(Metrics.tensor2img(visuals['SR'][-1]), hr_img)
         eval_ssim = Metrics.calculate_ssim(Metrics.tensor2img(visuals['SR'][-1]), hr_img)
-
+        Metrics.save_img(
+            Metrics.tensor2img(visuals['SR'][-1]), '{}/{}_{}_sr.png'.format('temp', 0, idx))
         avg_psnr += eval_psnr
         avg_ssim += eval_ssim
 
     avg_psnr = avg_psnr / idx
     avg_ssim = avg_ssim / idx
-    logger.info('# Validation # PSNR: {:.4e}'.format(avg_psnr))
-    logger.info('# Validation # SSIM: {:.4e}'.format(avg_ssim))
-    logger_val = logging.getLogger('val')  # validation logger
-    logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.4e}, ssim：{:.4e}'.format(
-        current_epoch, current_step, avg_psnr, avg_ssim))
+    # logger.info('# Validation # PSNR: {:.4e}'.format(avg_psnr))
+    # logger.info('# Validation # SSIM: {:.4e}'.format(avg_ssim))
+    # logger_val = logging.getLogger('val')  # validation logger
+    # logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.4e}, ssim：{:.4e}'.format(
+    #     current_epoch, current_step, avg_psnr, avg_ssim))
     return avg_psnr,avg_ssim
 def get_file_names(path):
     file_names = os.listdir(path)
@@ -108,16 +112,47 @@ def get_file_names(path):
 if __name__=='__main__':
     ckpt_path='/SR3/4x_diff_multry/Image-Super-Resolution-via-Iterative-Refinement-master/experiments/64_256_Train_0iter_230318_031257/checkpoint/'
     ckpt_list=get_file_names(ckpt_path)
-    print(len(ckpt_list))
+
+    t_dict={}
+    for k,epoch_name in enumerate(ckpt_list):
+        index_of_e = epoch_name.index('E')
+        epoch = epoch_name[index_of_e + 1:]
+        t_dict[(int(epoch))]=epoch_name
+    ckpt_list = {k: t_dict[k] for k in sorted(t_dict.keys())}.values()
+
     df=pd.DataFrame(columns=['PSNR','SSIM'])
-    for epoch_name in ckpt_list:
+    df_temp=pd.DataFrame(columns=['PSNR','SSIM'])
+    for k,epoch_name in enumerate(ckpt_list):
         index_of_e = epoch_name.index('E')
         epoch = epoch_name[index_of_e + 1:]
         print(epoch_name)
-        result_path='/SR3/Only_Infer_result/RESULT/'+str(index_of_e)
-        os.makedirs(result_path, exist_ok=True)
-        avg_psnr,avg_ssim=load_and_infer(epoch_name,result_path,1,10)
+        avg_psnr,avg_ssim=load_and_infer(ckpt_path+epoch_name,val_len=3)
         df.loc[epoch]=[avg_psnr,avg_ssim]
-    df.to_excel('/SR3/Only_Infer_result/RESULT/res.xlsx')
+        df_temp.loc[epoch]=[avg_psnr,avg_ssim]
+        if k%1==0:
+            df_temp.to_excel('/SR3/Only_Infer_result/DBPN_RESULT/res_{}.xlsx'.format(k))
+    df.to_excel('/SR3/Only_Infer_result/DBPN_RESULT/ffhq_100_0witer_res.xlsx')
 
-    #记得把base_model gpu还回去
+
+
+    ckpt_path='/SR3/4x_diff_multry/Image-Super-Resolution-via-Iterative-Refinement-master/experiments/64_256_Train_300000iter_230320_080415/checkpoint/'
+    ckpt_list=get_file_names(ckpt_path)
+    t_dict={}
+    for k,epoch_name in enumerate(ckpt_list):
+        index_of_e = epoch_name.index('E')
+        epoch = epoch_name[index_of_e + 1:]
+        t_dict[(int(epoch))]=epoch_name
+    ckpt_list = {k: t_dict[k] for k in sorted(t_dict.keys())}.values()
+
+    df=pd.DataFrame(columns=['PSNR','SSIM'])
+    df_temp=pd.DataFrame(columns=['PSNR','SSIM'])
+    for k,epoch_name in enumerate(ckpt_list):
+        index_of_e = epoch_name.index('E')
+        epoch = epoch_name[index_of_e + 1:]
+        print(epoch_name)
+        avg_psnr,avg_ssim=load_and_infer(ckpt_path+epoch_name,val_len=10)
+        df.loc[epoch]=[avg_psnr,avg_ssim]
+        df_temp.loc[epoch]=[avg_psnr,avg_ssim]
+        if k%1==0:
+            df_temp.to_excel('/SR3/Only_Infer_result/DBPN_RESULT_30w/res_{}.xlsx'.format(k))
+    df.to_excel('/SR3/Only_Infer_result/DBPN_RESULT_30w/ffhq_100_0witer_res.xlsx')
